abcc145d5b93ffc9c5974fadb9df17945cf68550
ramses2099 <ramses2099@gmail.com>
2025-03-03 20:56:39 -0500

    latest changes

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import fetch from "node-fetch";
 
+interface Post{
+    userId:number;
+    id:number;
+    title:string;
+    body:string;
+}
 
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(res => res.json())
-.then((data) =>console.log(data));
+.then((data: Post[]) => console.log(data[0]));
 
 
-console.log("testing");
